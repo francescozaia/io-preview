a6bebb440e54029f0d80127ffdf96f25dcb153c0
Francesco Zaia <francesco.zaia@gmail.com>
2018-04-13 00:38:17 +0200

    Responsive bits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 $(function () {
 
-    let staffPathArray = [];
+
+    var smallDevice = window.innerWidth < 992;
+
+    var staffPathArray = [];
 
     // Per ogni Path SVG creo un'animazione senza autoplay, che verrà animata secondo un setInterval
     $('.staff__path').each(function (index) {
@@ -14,14 +17,14 @@ $(function () {
             targets: '[data-index="' + pathID + '"] .staff__note',
             easing: 'easeInOutQuart',
             autoplay: false,
-            begin: function(anim) {
+            begin: function (anim) {
                 $('.ball--' + pathName).addClass('ball--burst');
                 $('[data-index="' + pathID + '"] .staff__note').removeClass('staff__note--burst');
             },
-            complete: function() {
+            complete: function () {
                 $('.ball--' + pathName).removeClass('ball--burst');
                 $('[data-index="' + pathID + '"] .staff__note').addClass('staff__note--burst');
-                $('[data-index="' + pathID + '"] .staff__note').fadeTo("slow",0);
+                $('[data-index="' + pathID + '"] .staff__note').fadeTo("slow", 0);
                 let audio = document.getElementById('ting');
                 audio.play();
             }
@@ -43,73 +46,83 @@ $(function () {
     // sfondo in parallasse
     var parallaxInstance = new Parallax($('.galaxy')[0]);
 
-    $('.page-scroll-container').fullpage({
-        responsiveWidth: 900,
-        anchors:['', 'messaggi', 'pagamenti', 'documenti', 'preferenze', 'profilo'],
-        //fitToSection: true,
-        css3: true,
-        fitToSection: false,
-        autoScrolling: false,
-        'onLeave': function (index, nextIndex, direction) {
+    var controller = new ScrollMagic.Controller();
 
-            if (index === 1 && direction === 'down') {
-                // Squeeze the Staff
-                //$('.staff').addClass('staff--squeeze');
-                // Add a Blue background to the Slim Navigation
-                $('.navigation__slim').addClass('navigation__slim--blue');
-                // Move the device down a bit to leave space for the Navigation
-                $('.device').addClass('device--down');
-                // Mute any sound
-                document.getElementById('ting').muted = true;
-                parallaxInstance.disable();
-            }
-            if (index === 2 && direction === 'up') {
-                //$('.staff').removeClass('staff--squeeze');
-                $('.navigation__slim').removeClass('navigation__slim--blue');
-                $('.device').removeClass('device--down');
-                document.getElementById('ting').muted = false;
-                parallaxInstance.enable();
-            }
+    new ScrollMagic.Scene({triggerElement: "#section_1"})
+        .on("leave", function () {
+            $(".device__content--1").removeClass("device__content--in");
+        });
 
-            $('div[class^="copy__content"]').removeClass('copy__content--in');
-            $('.copy__content--' + nextIndex).addClass('copy__content--in');
-            $('div[class^="device__content"]').removeClass('device__content--in');
-            $('.device__content--' + nextIndex).addClass('device__content--in');
-            $('div[class^="section-text"]').removeClass('in');
-            $('.section-text--' + nextIndex).addClass('in');
+        for (var _i = 2; _i < 7; _i++) {
+            var a = (function (i) {
+                new ScrollMagic.Scene({
+                    triggerElement: "#section_" + i,
+                    duration: window.innerHeight - 100
+                })
+                    .on("enter", function () {
+                        $("#section_" + i).addClass("active");
+                        $("#section_" + i + " .section-text").addClass("section-text__in");
 
+                            $(".device__content--" + i).addClass("device__content--in");
+
+                    })
+                    .on("leave", function () {
+                            $("#section_" + i).removeClass("active");
+                            $("#section_" + i + " .section-text").removeClass("section-text__in");
+                            $(".device__content--" + i).removeClass("device__content--in");
+                    }).addTo(controller); //.addIndicators({name:"section:" + i})
+            })(_i)
         }
-    });
 
     setInterval(function () {
 
-        // imposta una posizione random per il pallino verde
-        $('.italia__dot').fadeTo(400, 0).promise().done(function() {
-            $('.italia__dot').css({
-                backgroundPosition: Math.floor((Math.random() * document.body.clientWidth / 2)) + 'px ' + Math.floor((Math.random() * document.body.clientHeight)) + 'px'
-            }).fadeTo(400, 1);
-        });
-
-        // prende un Path SVG casuale nel pentagramma e ri-avvia l'animazione
-        staffPathArray[Math.floor(Math.random() * staffPathArray.length)].restart();
-
-    }, 5000);
-
-    $(window).on('scroll',function (event) {
-        if ($(window).scrollTop() > $('.section__one').height() - 50){
-            $('.navigation').addClass('navigation--top');
-            $('.section__two .section-wrapper').addClass('section-wrapper--margin-top');
-            $('.device').css({
-                'zIndex': -2
+        if (!smallDevice) {
+            var el = $('.italia__dot');
+            var w = el.width();
+            var h = el.height()
+            // imposta una posizione random per il pallino verde
+            el.fadeTo(400, 0).promise().done(function () {
+                el.css({
+                    backgroundPosition: Math.floor((Math.random() * w)) + 'px ' + Math.floor((Math.random() * h)) + 'px'
+                }).fadeTo(400, 1);
             });
+
+            // prende un Path SVG casuale nel pentagramma e ri-avvia l'animazione
+            staffPathArray[Math.floor(Math.random() * staffPathArray.length)].restart();
+        }
+    }, 2400);
+
+    $(window).on('scroll', function (event) {
+        if ($(window).scrollTop() > $('.section__1').height() - 50) {
+            $('.navigation').addClass('navigation--top');
+            $('.navigation__slim').addClass('navigation__slim--blue'); // TODO vedere per mobile
+            parallaxInstance.disable();
+            if (!smallDevice) {
+                $('.section__2 .section-wrapper').addClass('section-wrapper--margin-top');
+                $('.device').css({'zIndex': -2}); // TODO vedere per mobile
+                $('.device').addClass('device--down');
+                // Mute any sound
+                document.getElementById('ting').muted = true;
+            }
         } else {
             $('.navigation').removeClass('navigation--top');
-            $('.section__two .section-wrapper').removeClass('section-wrapper--margin-top');
-            $('.device').css({
-                'zIndex': 'auto'
-            });
+            $('.navigation__slim').removeClass('navigation__slim--blue'); // TODO vedere per mobile
+            parallaxInstance.enable();
+            if (!smallDevice) {
+                $('.section__2 .section-wrapper').removeClass('section-wrapper--margin-top');
+                $('.device').css({'zIndex': 'auto'}); // TODO vedere per mobile
+                $('.device').removeClass('device--down');
+                // Mute any sound
+                document.getElementById('ting').muted = false;
+            }
         }
     });
 
+    $(window).on('resize', function (event) {
+        smallDevice = window.innerWidth < 992;
+    });
 
-});
+// TODO debounce
+
+})
+;
