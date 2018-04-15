db67bfb867bf79586d93a0d378c694bfec61c7a6
Francesco Zaia <francesco.zaia@gmail.com>
2018-04-15 19:30:17 +0200

    Nuovi suoni, revisione codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,118 +1,121 @@
 $(function () {
 
+    let smallDevice = window.innerWidth < 992;
 
-    var smallDevice = window.innerWidth < 992;
+    let carillonPathArray = [];
 
-    var staffPathArray = [];
+    // Per ogni Path SVG esiste un'animazione senza autoplay
+    $('.carillon__path').each(function (index) {
 
-    // Per ogni Path SVG creo un'animazione senza autoplay, che verrà animata secondo un setInterval
-    $('.staff__path').each(function (index) {
-
-        $element = $(this);
         let pathID = $(this).data('index');
+        let soundID = $(this).data('sound');
         let pathName = $(this).data('name');
         let path = anime.path('[data-index="' + pathID + '"] path');
 
         let animeTimeline = anime.timeline({
-            targets: '[data-index="' + pathID + '"] .staff__note',
+            targets: '[data-index="' + pathID + '"] .carillon__note',
             easing: 'easeInOutQuart',
             autoplay: false,
             begin: function (anim) {
                 $('.ball--' + pathName).addClass('ball--burst');
-                $('[data-index="' + pathID + '"] .staff__note').removeClass('staff__note--burst');
+                $('[data-index="' + pathID + '"] .carillon__note').removeClass('carillon__note--burst');
             },
             complete: function () {
                 $('.ball--' + pathName).removeClass('ball--burst');
-                $('[data-index="' + pathID + '"] .staff__note').addClass('staff__note--burst');
-                $('[data-index="' + pathID + '"] .staff__note').fadeTo("slow", 0);
-                let audio = document.getElementById('ting');
-                audio.play();
+                $('[data-index="' + pathID + '"] .carillon__note').addClass('carillon__note--burst').fadeTo("slow", 0);
+                document.getElementById('sound-0' + soundID).play();
             }
         });
 
         animeTimeline.add({
             opacity: 1,
-            duration: 1000
+            duration: 1000,
+            delay: $(this).data('delay')
         }).add({
             opacity: 1,
             translateX: path('x'),
             translateY: path('y'),
-            duration: $(this).data('duration')
+            duration: 1000
         });
 
-        staffPathArray.push(animeTimeline);
+        carillonPathArray.push(animeTimeline);
     });
 
-    // sfondo in parallasse
-    var parallaxInstance = new Parallax($('.galaxy')[0]);
+    // Sfondo in parallasse
+    let parallaxInstance = new Parallax($('.galaxy')[0]);
 
-    var controller = new ScrollMagic.Controller();
+    // ScrollMagic
+    let controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({triggerElement: "#section_1"})
-        .on("leave", function () {
-            $(".device__content--1").removeClass("device__content--in");
-        });
-
-        for (var _i = 2; _i < 7; _i++) {
-            var a = (function (i) {
-                new ScrollMagic.Scene({
-                    triggerElement: "#section_" + i,
-                    duration: window.innerHeight - 100
-                })
-                    .on("enter", function () {
-                        $("#section_" + i).addClass("active");
-                        $("#section_" + i + " .section-text").addClass("section-text__in");
-
+    // Sezioni 2, 3, 4, 5, 6
+    for (let _i = 2; _i < 7; _i++) {
+        (function (i) {
+            new ScrollMagic.Scene({
+                triggerElement: "#section_" + i,
+                duration: window.innerHeight - 100 // TODO da rivedere
+            })
+                .on("enter", function () {
+                    if (i !== 1) {
+                        if (!smallDevice) {
+                            $(".section__" + i).addClass("active");
                             $(".device__content--" + i).addClass("device__content--in");
+                        }
+                    }
+                })
+                .on("leave", function () {
+                    if (!smallDevice) {
+                        $(".section__" + i).removeClass("active");
+                        $(".device__content--" + i).removeClass("device__content--in");
+                    }
+                }).addTo(controller);
+        })(_i);
+    }
 
-                    })
-                    .on("leave", function () {
-                            $("#section_" + i).removeClass("active");
-                            $("#section_" + i + " .section-text").removeClass("section-text__in");
-                            $(".device__content--" + i).removeClass("device__content--in");
-                    }).addTo(controller); //.addIndicators({name:"section:" + i})
-            })(_i)
-        }
-
-    setInterval(function () {
-
-        if (!smallDevice) {
-            var el = $('.italia__dot');
-            var w = el.width();
-            var h = el.height();
-            // imposta una posizione random per il pallino verde
-            el.fadeTo(400, 0).promise().done(function () {
+    let greenDotAnimation = function () {
+        let el = $('.italia__dot');
+        el.fadeTo(200, 0)
+            .promise()
+            .done(function () {
                 el.css({
-                    backgroundPosition: Math.floor((Math.random() * w)) + 'px ' + Math.floor((Math.random() * h)) + 'px'
-                }).fadeTo(400, 1);
+                    backgroundPositionX: Math.floor((Math.random() * el.width())) + 'px',
+                    backgroundPositionY: Math.floor((Math.random() * el.height())) + 'px'
+                }).fadeTo(200, 1);
             });
+    };
 
-            // prende un Path SVG casuale nel pentagramma e ri-avvia l'animazione
-            staffPathArray[Math.floor(Math.random() * staffPathArray.length)].restart();
+    (function loopingFunction() {
+
+        setTimeout(loopingFunction, 1000);
+    })();
+
+    let lastTime = 0;
+
+    let animate = function (currentTime) {
+        if (currentTime >= lastTime + 1000) {
+            // one second has passed, run some code here
+            if (!smallDevice) {
+                if (!$('body').hasClass('is-scrolled')) {
+                    // prende un Path SVG casuale nel carillon e ri-avvia l'animazione
+                    carillonPathArray[Math.floor(Math.random() * carillonPathArray.length)].restart();
+                } else {
+                    // Imposta una posizione casuale la notifica "dot" verde
+                    greenDotAnimation();
+                }
+            }
+            lastTime = currentTime;
         }
-    }, 2400);
+        requestAnimationFrame(animate);
+    };
 
-    $(window).on('scroll', function (event) {
+    animate(0);
+
+    $(window).on('load scroll', function (event) {
         if ($(window).scrollTop() > $('.section__1').height() - 50) {
-            $('.navigation').addClass('navigation--top');
+            $('body').addClass('is-scrolled');
             parallaxInstance.disable();
-            if (!smallDevice) {
-                $('.navigation__slim').addClass('navigation__slim--blue');
-                $('.section__2 .section-wrapper').addClass('section-wrapper--margin-top');
-                $('.device').css({'zIndex': -2}); // TODO vedere per mobile
-                $('.device').addClass('device--down');
-                document.getElementById('ting').muted = true;
-            }
         } else {
-            $('.navigation').removeClass('navigation--top');
+            $('body').removeClass('is-scrolled');
             parallaxInstance.enable();
-            if (!smallDevice) {
-                $('.navigation__slim').removeClass('navigation__slim--blue');
-                $('.section__2 .section-wrapper').removeClass('section-wrapper--margin-top');
-                $('.device').css({'zIndex': 1}); // TODO vedere per mobile
-                $('.device').removeClass('device--down');
-                document.getElementById('ting').muted = false;
-            }
         }
     });
 
@@ -120,7 +123,4 @@ $(function () {
         smallDevice = window.innerWidth < 992;
     });
 
-// TODO debounce
-
-})
-;
+});
